13dd54e275962472e8934354633cb15a9943e1ce
Peter Horvath <horvthpeter@gmail.com>
2021-12-27 02:41:41 +0200

    Add dynamic websocket server.

--- main.js
+++ main.js
@@ -60,13 +60,24 @@ function receiveMoves(board, websocket) {
   });
 }
 
+function getWebsocketServer() {
+  if (window.location.host == 'pisskidney.github.io') {
+    return 'wss://snakemmo.herokuapp.com/';
+  }
+  else if (window.location.host == 'localhost:8000') {
+    return 'ws://localhost:8001/'
+  else {
+    throw new Error(`Unsupported host: ${window.location.host}`);
+  }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   // Initialize the UI.
   const board = document.querySelector(".board");
   createBoard(board);
 
   // Open the WebSocket connection and register event handlers.
-  const websocket = new WebSocket("ws://localhost:8001/");
+  const websocket = new WebSocket(getWebsocketServer());
   initGame(websocket);
   receiveMoves(board, websocket);
   sendMoves(board, websocket);
